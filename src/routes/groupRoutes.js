766f979fe9472b77b5e037e069f6aee6e2956778
AiSharath <sharathbhat755@gmail.com>
2026-07-19 21:57:35 +0530

    Added the route to group

---
+++ src/routes/groupRoutes.js
@@ -0,0 +1,12 @@
+const express=require("express")
+const {createGroup,updateGroup,deleteGroup}=require("../controllers/groupController.js")
+const authMiddleware = require('../middleware/authMiddleware')
+
+const router=express.Router()
+router.use(authMiddleware)
+
+router.post("/create",createGroup)
+router.put("/:id",updateGroup)
+router.delete("/:id",deleteGroup)
+
+module.exports=router
